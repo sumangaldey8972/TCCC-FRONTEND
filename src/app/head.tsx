23bd03fd60dc4ca71919b35a6e2746be70753868
sumangaldey8972 <sumangaldey8972@gmail.com>
2025-12-10 12:05:52 +0530

    meta tag moved to head.tsx file

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "The Coin Cartel",
+    other: {
+        "the-coin-cartel": "24cda809b6ff"
+    }
+};
+
 export default function Head() {
     return (
         <>
-            <meta name="the-coin-cartel" content="69a83bd23e0f" />
+            <meta name="the-coin-cartel" content="24cda809b6ff" />
         </>
-    )
+    );
 }
